27725ea6c82163393dc50b5d3010cc5cce906d26
Peter Good <pwggithub@gmail.com>
2024-10-11 12:01:39 -0400

    remove booster

--- js/nav.js
+++ js/nav.js
@@ -7,7 +7,7 @@ nav.innerHTML = `
         <li class="nav-item"> <a class="nav-link" href="week.html"><i class="fa-solid fa-chart-column"></i></a></li>
         <li class="nav-item"> <a class="nav-link" href="yearly.html"><i class="fa-solid fa-trophy"></i></a></li>
         <li class="nav-item"> <a class="nav-link" href="weight.html"><i class="fa fa-weight"></i></a></li>
-		<li class="nav-item"> <a class="nav-link" href="Boosters.html"><i class="fa fa-rocket"></i></a></li>
+		<!--<li class="nav-item"> <a class="nav-link" href="Boosters.html"><i class="fa fa-rocket"></i></a></li>-->
        
       
       </ul>
