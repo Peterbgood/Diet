e0bdd0a22237c958ae916eff83df7cc1306da041
Peter Good <pwggithub@gmail.com>
2024-11-09 15:45:45 -0500

    update

--- js/week.js
+++ js/week.js
@@ -127,12 +127,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const totalAllowed = 11200; 
     const totalConsumedCalories = weeks[currentWeekIndex].reduce((a, b) => a + b, 0);
     const remainingCalories = totalAllowed - totalConsumedCalories;
-    totalCalories.textContent = `Remaining Cals for Week ${currentWeekIndex + 1}: ${remainingCalories}`;
+    totalCalories.textContent = `Remaining: ${remainingCalories}`;
   }
 
   function updateWeeklyTotal() {
     const totalConsumedCalories = weeks[currentWeekIndex].reduce((a, b) => a + b, 0);
-    weeklyTotal.textContent = `Cals Consumed for Week ${currentWeekIndex + 1}: ${totalConsumedCalories}`;
+    weeklyTotal.textContent = `Consumed: ${totalConsumedCalories}`;
   }
 
   function updateTotalSaved() {
@@ -146,7 +146,7 @@ document.addEventListener('DOMContentLoaded', () => {
         totalSavedCalories -= Math.abs(dailyCaloriesSaved);
       }
     });
-    totalSaved.textContent = `Cals Saved for Week ${currentWeekIndex + 1}: ${totalSavedCalories}`;
+    totalSaved.textContent = `Saved: ${totalSavedCalories}`;
   }
 
   function updatePieChart() {
