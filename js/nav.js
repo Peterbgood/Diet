e08954f65097f5025618fd0bf97170e5c4aaf090
Peterbgood <peterwgood77@gmail.com>
2024-12-22 08:20:15 -0500

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -3,7 +3,7 @@ nav.innerHTML = `
   <nav class="navbar bg-light">
     <div class="container-fluid">
        <ul class="navbar-nav justify-content-center">
-        <li class="nav-item"> <a class="nav-link" href="index.html"><i class="fa-solid fa-utensils"></i></a></li>
+        <li class="nav-item"> <a class="nav-link" href="https://peterbgood.github.io/React-Calorie-App/"><i class="fa-solid fa-utensils"></i></a></li>
         <li class="nav-item"> <a class="nav-link" href="week.html"><i class="fa-solid fa-chart-column"></i></a></li>
         <li class="nav-item"> <a class="nav-link" href="yearly.html"><i class="fa-solid fa-trophy"></i></a></li>
         <li class="nav-item"> <a class="nav-link" href="weight.html"><i class="fa fa-weight"></i></a></li>
